100be9305d631d66d9c1d72878656cb044238504
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2021-11-28 13:22:25 +0000

    Added user filtering to load_ratings

---
+++ load_ratings.ps1
@@ -0,0 +1,5 @@
+$o = $args[0]
+$t = $args[1]
+$s = $args[2]
+$e = $args[3]
+python ./scripts/load_ratings.py $o -t $t -s $s -e $e -v
--- scripts/load_ratings.py
+++ scripts/load_ratings.py
@@ -6,7 +6,7 @@ from time import time
 from utils import save_matrix, str_to_uid
 
 
-def load_ratings(start_page, end_page, threshold, verbose):
+def load_ratings(start_page=1, end_page=524, threshold=50, verbose=True):
     # logging
     print(f'Loading ratings for pages {start_page}-{end_page}')
     t = time()
@@ -51,19 +51,37 @@ def load_ratings(start_page, end_page, threshold, verbose):
         if num_ratings[game_idx] >= threshold:
             filt_game_map[game_idx] = filt_game_count
             filt_game_count += 1
-    # logging
     print(f'Ignoring {game_count - filt_game_count} games')
+    # remove users without any included reviews
+    valid_user_ids = set()
+    for user_idx in range(user_count):
+        any_valid = False
+        for ratings in user_ratings[user_idx]:
+            if any_valid: break
+            for game_idx in ratings:
+                if game_idx in filt_game_map:
+                    any_valid = True
+                    break
+        if any_valid:
+            valid_user_ids.add(user_idx)
+    filt_user_count = len(valid_user_ids)
+    # logging
+    print(f'Ignoring {user_count - filt_user_count} users')
     print(f'Loaded games in {int(time() - t)} seconds')
-    print(f'Generating matrix ({user_count} x {filt_game_count})')
+    print(f'Generating matrix ({filt_user_count} x {filt_game_count})')
     t = time()
     # convert data into sparse matrix and save
-    matrix = sparse.lil_matrix((user_count, filt_game_count))
+    matrix = sparse.lil_matrix((filt_user_count, filt_game_count))
+    filt_user_idx = 0
     for user_idx in range(user_count):
+        if user_idx not in valid_user_ids:
+            continue
         for polarity, ratings in zip([-1, 1], user_ratings[user_idx]):
             for game_idx in ratings:
                 if game_idx in filt_game_map:
                     filt_game_idx = filt_game_map[game_idx]
-                    matrix[user_idx, filt_game_idx] = polarity
+                    matrix[filt_user_idx, filt_game_idx] = polarity
+        filt_user_idx += 1
     # logging
     print(f'Generated matrix in {int(time() - t)} seconds')
     return matrix
@@ -72,10 +90,16 @@ def load_ratings(start_page, end_page, threshold, verbose):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('out_file', help='matrix output file')
     parser.add_argument('-s', '--start_page', type=int, default=1, help='review page to start at')
     parser.add_argument('-e', '--end_page', type=int, default=524, help='review page to end at (inclusive)')
     parser.add_argument('-t', '--threshold', type=int, default=50, help='minimum number of reviews for game to be included')
     parser.add_argument('-v', '--verbose', action='store_true', help='output detailed progress')
     args = parser.parse_args()
-    matrix = load_ratings(args.start_page, args.end_page, args.threshold, args.verbose)
-    save_matrix(f'ratings_{args.threshold}_{args.start_page}-{args.end_page}', matrix)
+    matrix = load_ratings(
+        start_page=args.start_page,
+        end_page=args.end_page,
+        threshold=args.threshold,
+        verbose=args.verbose
+    )
+    save_matrix(args.out_file, matrix)
